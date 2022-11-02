94213af3ea83a95beb02605cce2c11f4136efeec
Esk1z0 <j.esteban.rincon.m@gmail.com>
2022-11-02 10:23:19 +0100

    first commit

---
+++ principal.py
@@ -0,0 +1,37 @@
+from heuristicSearch import heuristicSearch
+
+if __name__ == "__main__":
+    direccion = "/Users/jeste/Desktop/Clase/Segundo/IA/Tema_2_ExploracionEstados/dataset/Vienna_metro"
+    h = heuristicSearch()
+    h.initGraph(direccion, "Vienna subway.csv", "Start", "Stop", sepa= ";")
+    print(h)
+    
+    h.set_PrintProcess = False
+    h.set_origin = "Oberlaa"
+    h.set_dest = "Leopoldau"
+    
+    #print(h.origin)
+    #print(h.dest)
+    
+    resul = h.depth()
+    print(resul)
+    resulStr = ""
+    for i in resul:
+        if (not i == resul[-1]):
+            resulStr += h.getStation(i) + " --> "
+        else:
+            resulStr += h.getStation(i)
+    print(resulStr)
+    
+    resul = h.amplitude()
+    print(resul)
+    resulStr = ""
+    for i in resul:
+        if (not i == resul[-1]):
+            resulStr += h.getStation(i) + " --> "
+        else:
+            resulStr += h.getStation(i)
+    print(resulStr)
+    
+    linea1 = [0, 9, 10, 11, 13, 94]
+    h.insertMetroLines(linea1, "RED")
